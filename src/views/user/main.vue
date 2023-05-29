f18c7831173de944aa3b2d7290e0491c99f25cef
wzw <zhengwei_wang@lebo.cn>
2023-05-29 15:37:12 +0800

    feat(deps): 项目初始化

---
+++ src/views/user/main.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="main">
+    主页
+    <span>hahahh</span>
+  </div>
+</template>
+
+<script setup lang="ts" name="dashboard">
+import { reactive } from "vue";
+</script>
+
+<style scoped lang="scss">
+.main {
+  color: aqua;
+  span {
+    color: rgb(67, 171, 171);
+  }
+}
+</style>
